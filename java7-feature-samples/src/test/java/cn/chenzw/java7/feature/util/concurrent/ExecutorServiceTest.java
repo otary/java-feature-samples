eaef88d1f5cb40804dea47f47f6c5bbbe7fc253a
chenzw <656469722@qq.com>
2020-01-15 16:24:37 +0800

    initialization

--- java7-feature-samples/src/test/java/cn/chenzw/java7/feature/util/concurrent/ExecutorServiceTest.java
+++ java7-feature-samples/src/test/java/cn/chenzw/java7/feature/util/concurrent/ExecutorServiceTest.java
@@ -6,9 +6,8 @@ import org.junit.runners.JUnit4;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(JUnit4.class)
 public class ExecutorServiceTest {
@@ -16,6 +15,11 @@ public class ExecutorServiceTest {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * 定时、延迟线程池
+     *
+     * @throws InterruptedException
+     */
     @Test
     public void testScheduledExecutorService() throws InterruptedException {
 
@@ -42,6 +46,48 @@ public class ExecutorServiceTest {
             TimeUnit.SECONDS.sleep(1);
         }
 
+    }
+
+    /**
+     * 定时执行（异常抛出）
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testScheduledThreadPoolExecutorWithException() throws InterruptedException {
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1, new ThreadFactory() {
+            private final AtomicInteger threadId = new AtomicInteger(1);
+
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r, "my-schedule-thread-" + threadId.getAndIncrement());
+                logger.info("Create new thread [{}]!", thread.getName());
+                return thread;
+            }
+        });
+
+        AtomicInteger count = new AtomicInteger(1);
+
+        // 内部任务异常需要进行捕获处理，否则定时任务会退出
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("Schedule [{}]", count.get());
+
+                try {
+                    if (count.getAndIncrement() % 5 == 0) {
+                        throw new RuntimeException("抛出异常!");
+                    }
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (Exception e) {
+                    logger.error("Schedule with exception!", e);
+                }
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+
+        while (!scheduledThreadPoolExecutor.isTerminated()) {
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
 
