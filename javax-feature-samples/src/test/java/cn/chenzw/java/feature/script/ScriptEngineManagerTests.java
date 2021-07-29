91f3c82eff2eeec815c0eed4bbf39f24813849c4
chenzw <656469722@qq.com>
2021-07-29 15:05:44 +0800

    update

--- javax-feature-samples/src/test/java/cn/chenzw/java/feature/script/ScriptEngineManagerTests.java
+++ javax-feature-samples/src/test/java/cn/chenzw/java/feature/script/ScriptEngineManagerTests.java
@@ -7,7 +7,6 @@ import org.junit.runners.JUnit4;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -50,6 +49,62 @@ public class ScriptEngineManagerTests {
 
         // 读取变量
         logger.info("fS_name: {}", engine.get("fS_name"));
+    }
+
+    /**
+     * 混淆Js测试
+     *
+     * @throws ScriptException
+     * @throws FileNotFoundException
+     */
+    @Test
+    public void testConfuison() throws ScriptException, FileNotFoundException {
+        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+        ScriptEngine engine = scriptEngineManager.getEngineByName("JavaScript");
+
+        FileReader fileReader = new FileReader(Thread.currentThread().getContextClassLoader().getResource("js/confusion.txt").getFile());
+        engine.eval(fileReader);
+
+        ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) engine.get("__NUXT__");
+
+
+        ScriptObjectMirror data = (ScriptObjectMirror) scriptObjectMirror.get("data");
+        for (int i = 0; i < data.size(); i++) {
+            ScriptObjectMirror slot = (ScriptObjectMirror) data.getSlot(i);
+
+            ScriptObjectMirror dataList = (ScriptObjectMirror) slot.get("dataList");
+
+            for (int j = 0; j < dataList.size(); j++) {
+                ScriptObjectMirror dataItem = (ScriptObjectMirror) dataList.getSlot(j);
+
+                String description = (String) dataItem.get("description");
+
+                String coverImage = (String) dataItem.get("cover_image_url");
+                String image = (String) dataItem.get("vertical_image_url");
+
+                String title = (String) dataItem.get("title");
+
+                // 分类
+                ScriptObjectMirror category = (ScriptObjectMirror) dataItem.get("category");
+                for (int k = 0; k < category.size(); k++) {
+
+                }
+
+                // 更新状态
+                String updateStatus = (String) dataItem.get("update_status");
+
+
+
+
+                System.out.println(description);
+
+
+            }
+
+            //  System.out.println(slot.get("count"));
+        }
+
+
     }
 
 }
