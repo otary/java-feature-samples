d7010515050c42be766ad8f4958c4d82fb115d14
chenzw <656469722@qq.com>
2021-08-09 17:08:14 +0800

    update

--- java8-feature-samples/src/test/java/cn/chenzw/java8/feature/time/LocalDateTest.java
+++ java8-feature-samples/src/test/java/cn/chenzw/java8/feature/time/LocalDateTest.java
@@ -7,8 +7,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
 
 /**
  * 日期
@@ -114,12 +118,60 @@ public class LocalDateTest {
         Assert.assertEquals("2019-11-03", date.plus(1, ChronoUnit.MONTHS).plus(2, ChronoUnit.DAYS).toString());
     }
 
+
+    /**
+     * 解析日期
+     */
     @Test
     public void testParse() {
         // 只能解析日期，不能带时间
         LocalDate date = LocalDate.parse("2021-10-01");
-
         log.info(" => {}", date);
+
+        // 解析报错（默认只解析yyyy-MM-dd格式）
+        //LocalDate date2 = LocalDate.parse("2021/10/01");
+
+
+        // 自定义解析格式
+        LocalDate date3 = LocalDate.parse("2021/10/01", DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        log.info("自定义解析格式 => {}", date3);
+
+    }
+
+    /**
+     * LocalDate 转 Date对象
+     */
+    @Test
+    public void testLocalDate2Date() {
+        LocalDate today = LocalDate.now();
+        Date date = Date.from(today.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        log.info("=> {}", date);
+    }
+
+    /**
+     * Date 转 LocalDate对象
+     */
+    @Test
+    public void testDate2LocalDate() {
+
+    }
+
+
+    /**
+     * 获取第一天、最后一天
+     */
+    @Test
+    public void testTemporalAdjusters() {
+        LocalDate today = LocalDate.now();
+
+        // 月份第一天
+        LocalDate firstDayOfMonth = today.with(TemporalAdjusters.firstDayOfMonth());
+        log.info("firstDayOfMonth => {}", firstDayOfMonth);
+
+        // 月份最后一天
+        LocalDate lastDayOfMonth = today.with(TemporalAdjusters.lastDayOfMonth());
+        log.info("lastDayOfMonth => {}", lastDayOfMonth);
     }
 
 
