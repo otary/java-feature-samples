fdcef36592113691570b8664397f6b1b452851b6
chenzw <656469722@qq.com>
2021-09-09 17:33:24 +0800

    update

---
+++ javax-feature-samples/src/test/java/cn/chenzw/java/feature/lang/DoubleTests.java
@@ -0,0 +1,47 @@
+package cn.chenzw.java.feature.lang;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Calendar;
+
+@RunWith(JUnit4.class)
+public class DoubleTests {
+
+    @Test
+    public void testCompare() {
+        Double a1 = new Double("10.234");
+        Double b1 = new Double("11");
+        Assert.assertTrue(a1 < b1);
+        Assert.assertTrue(a1.compareTo(b1) < 0);
+
+        Double a2 = new Double("10.234");
+        Double b2 = new Double("10.2340");
+        Assert.assertTrue(a2.compareTo(b2) == 0);
+        Assert.assertFalse(a2 == b2); // 不能用等于号
+
+        Double a3 = new Double("3");
+        Double b3 = new Double("4");
+        Assert.assertTrue((a3 * b3) == 12);
+        Assert.assertTrue(new Double("12").compareTo(a3 * b3) == 0);
+
+        Double t = new Double("10");
+        Double s = new Double("12");
+        Double x = new Double("3");
+
+        System.out.println((s - t)/t * 100 / 3);
+
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        for (int i = 1; i <= 12; i++) {
+            String month = year + StringUtils.leftPad(String.valueOf(i), 2, "0");
+
+            System.out.println(month);
+        }
+
+    }
+}
